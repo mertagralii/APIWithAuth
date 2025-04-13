f12b585c72303888095bb2204bb08e28f8ae635b
Mert Ağralı <my_mert07hotmail.com>
2025-04-13 22:20:46 +0300

    Identity

---
+++ Program.cs
@@ -0,0 +1,74 @@
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using APIWithAuth.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIWithAuth;
+public sealed class SlugifyParameterTransformer : IOutboundParameterTransformer
+{
+    public string? TransformOutbound(object? value)
+    {
+        if (value == null) { return null; }
+        string? str = value.ToString();
+        if (string.IsNullOrEmpty(str)) { return null; }
+
+        return Regex.Replace(str, "([a-z])([A-Z])", "$1-$2").ToLowerInvariant();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services
+            .AddControllers(options =>
+            {
+                options.Conventions.Add(
+                    new RouteTokenTransformerConvention(new SlugifyParameterTransformer()));
+            })
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAuthorization();
+        builder.Services.Configure<IdentityOptions>(options =>
+        { 
+            // Burada istediğimiz ayarlamalarımızı yapıyoruz.
+            options.SignIn.RequireConfirmedAccount = false;
+            options.Password.RequiredLength = 3;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        });
+        builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        // identity'lerin enpointlerin başına ekleme yapmak için
+        //prefix ön ekle suffix son ek
+        app.MapGroup("/user").MapIdentityApi<IdentityUser>();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
